8cba2cfa3c2587b017156c3eef6da981a9f81ed6
Rammyres Pereira <rammyres@gmail.com>
2023-10-05 00:44:22 -0300

    Alterações na aparência da Lista

--- lib/ciomponentes/card_atracao.dart
+++ lib/ciomponentes/card_atracao.dart
@@ -12,35 +12,39 @@ class CardAtracao extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.bottomRight,
-      children: [
-        Image.network(
-          fotoUrl,
-          height: 200.0,
-          width: double.infinity,
-          fit: BoxFit.cover,
-        ),
-        Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.centerLeft,
-              end: Alignment.centerRight,
-              colors: [Colors.transparent, Colors.blue.withOpacity(0.7)],
+    return Container(
+      padding: EdgeInsets.all(8),
+      child: Stack(
+        alignment: Alignment.bottomRight,
+        children: [
+          Image.network(
+            fotoUrl,
+            height: 200.0,
+            width: double.infinity,
+            fit: BoxFit.cover,
+          ),
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
+                colors: [Colors.transparent, Colors.blue.withOpacity(0.7)],
+              ),
+            ),
+            padding:
+                const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+            child: Text(
+              nome,
+              style: const TextStyle(
+                fontSize: 18.0,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.left,
             ),
           ),
-          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-          child: Text(
-            nome,
-            style: const TextStyle(
-              fontSize: 18.0,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-            ),
-            textAlign: TextAlign.left,
-          ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
--- lib/telas/tela_inicial.dart
+++ lib/telas/tela_inicial.dart
@@ -1,36 +1,72 @@
 import 'package:flutter/material.dart';
 import '../dados/atracoes.dart';
 import '../ciomponentes/card_atracao.dart'; // Importe o widget CardAtracao
+import '../modelos/atracao.dart';
 import '../telas/tela_detalhes_atracao.dart'; // Importe a tela DetalhesAtracaoScreen
 
-class TelaInicial extends StatelessWidget {
+class TelaInicial extends StatefulWidget {
   const TelaInicial({super.key});
 
   @override
+  _TelainicialState createState() => _TelainicialState();
+}
+
+class _TelainicialState extends State<TelaInicial> {
+  String filtro = ""; // Variável para armazenar o texto de filtragem
+
+  @override
   Widget build(BuildContext context) {
+    // Filtra as atrações com base no texto de filtragem
+    List<Atracao> atracoesFiltradas = atracoes.where((atracao) {
+      final nomeAtracao = atracao.nome.toLowerCase();
+      return nomeAtracao.contains(filtro.toLowerCase());
+    }).toList();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Visite Campo Maior'),
       ),
-      body: ListView.builder(
-        itemCount: atracoes.length,
-        itemBuilder: (ctx, index) {
-          final atracao = atracoes[index];
-          return GestureDetector(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => DetalhesAtracaoScreen(atracao: atracao),
-                ),
-              );
-            },
-            child: CardAtracao(
-              nome: atracao.nome,
-              fotoUrl: atracao.foto,
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              onChanged: (value) {
+                setState(() {
+                  filtro = value;
+                });
+              },
+              decoration: const InputDecoration(
+                labelText: 'Filtrar por nome',
+                hintText: 'Digite o nome da atração',
+                prefixIcon: Icon(Icons.search),
+              ),
             ),
-          );
-        },
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: atracoesFiltradas.length,
+              itemBuilder: (ctx, index) {
+                final atracao = atracoesFiltradas[index];
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            DetalhesAtracaoScreen(atracao: atracao),
+                      ),
+                    );
+                  },
+                  child: CardAtracao(
+                    nome: atracao.nome,
+                    fotoUrl: atracao.foto,
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }

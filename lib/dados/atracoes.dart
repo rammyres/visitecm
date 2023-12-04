52b5ad15b6b181cae314a63073fe98eb98ce5a5b
Rammyres Pereira <rammyres@gmail.com>
2023-12-03 23:58:36 -0300

    Inclusão de novos ondecebs Alteração do iconset do App

--- lib/dados/atracoes.dart
+++ lib/dados/atracoes.dart
@@ -11,9 +11,8 @@ final atracoes = [
       "https://www.gp1.com.br/media/image_bank/2023/6/acude-de-campo-maior-vai-passar-por-revitalizacaonone.jpg.950x0_q95_crop.webp",
     ],
     descricao: [
+      "O Açude Grande destaca-se como uma beleza natural em Campo Maior, Piauí.Ponto de referência na cidade. Quem chega à Terra dos Carnaubais, que chega pela BR-343, depara-se com um panorama espetacular em suas águas serenas proporcionam um ambiente propício para atividades recreativas, É um local aprazível e próprio à pratica de esportes ,passeios prosaicos são os mais procurados por pessoas que fazem caminhadas nas passarelas que, ornamentadas de carnaúbas, enfeitam a cidade. Além de possuir um belo pôr do sol.",
       "O Açude Grande de Campo Maior foi construido entre 1858 e 1859, em obra realida pelo Cel. Jacob Almendra.",
-      "Durante anos foi foco de atenção da cidade de Campo Maior, tendo sido as suas margens instalada a conhecida 'Praça da Radar', posteriormente reformada para o Complexo Cultural Valdir Fortes e o restaurante Hawaii.",
-      "O Açude também é o local onde o campomaiorense se exercita, caminhando ao redor de sua orla.",
     ],
     localizacao:
         "https://www.google.com/maps/@-4.8356205,-42.1676946,17.04z?entry=ttu",
@@ -23,9 +22,12 @@ final atracoes = [
     nome: "Pé da Serra",
     fotos: [
       "assets/imagens/tur2_1.png",
+      "assets/imagens/tur2_2.png",
+      "assets/imagens/tur2_3.png",
     ],
     descricao: [
-      "O Pé da Serra de Campo Maior é um conjunto de formações rochosas que abriga morros, piscinas naturais, rios e cachoeiras. Oficialmente é conhecido como Serra de Santo Antônio.",
+      "Para os amantes da natureza e trilhas, a Serra de Santo Antônio é um destino imperdível. Oferece vistas panorâmicas deslumbrantes, proporcionando uma experiência única para aqueles que exploram suas trilhas e apreciam a beleza natural da região.Localiza-se à aproximadamente 15 km do centro da cidade, no sentido da estrada que vai para o município de Coivaras.",
+      "È considerado outro ponto de visita obrigatório que é atração pelos arredores da cidade. Surpreende pelos banhos de água cristalina e gelada em suas piscinas naturais que enchem em época de inverno",
       "Em um de seus morros foi instalado um mirante e um crucifixo, acessível por uma escadaria que serve como 'trilha'.",
     ],
     localizacao:
@@ -41,7 +43,8 @@ final atracoes = [
       "assets/imagens/tur3_4.png",
     ],
     descricao: [
-      "O monumento que marca o local do combate contra as tropas portuguesas, única sangrenta no processo de independência do Brasil.",
+      " O Monumento do Jenipapo é uma homenagem histórica à Batalha do Jenipapo, um episódio relevante na luta pela independência do Brasil. Este monumento destaca-se como um símbolo memorial, convidando os visitantes a refletirem sobre o passado e a importância desse evento na história nacional.",
+      "Erguido em 1975, inaugurado pelo governador Alberto Silva, está aproximadamente à 07 km do centro da cidade, situado à margem esquerda da BR-343 no sentido Campo Maior/Parnaíba, próximo ao rio Jenipapo. È considerado o patrimônio histórico mais antigo da cidade, onde testemunha a bravura dos que, com seu sangue, preservaram a unidade nacional e deram eloqüente exemplo de brasilidade às futuras gerações",
       "Junto ao monumento existe também o Cemitério do Batalhão e o Museu do Couro.",
     ],
     localizacao: "https://www.google.com/maps/@-4.7823835,-42.1184066,16z",
@@ -53,8 +56,8 @@ final atracoes = [
       "assets/imagens/tur5_1.png",
     ],
     descricao: [
-      "As barragens Emparedado e Corredores se localizam no Povoado Cacimbas em Campo Maior.",
-      "Ambas as barragens são conhecidas por atrair muitos visitantes durante o período chuvoso, já que fornecem opções acessíveis de banhos.",
+      "A Barragem dos Corredores não só desempenha um papel vital na gestão hídrica da região, mas também é um ponto de interesse devido à sua imponência. Os visitantes podem apreciar a grandiosidade da estrutura e desfrutar das paisagens ao seu redor.",
+      "Possui uma extensão de 22 km de espelho d’água cristalina. Esta localizada a aproximadamente 46 quilômetros do centro da cidade. Para chegar lá você deve trafegar na PI-115 no sentido Campo Maior/Castelo do Piauí,É excelente para a prática de esportes náuticos e pescarias. Lá se pode encontrar o tambaqui, que é um peixe típico de criatórios da região.",
     ],
     localizacao:
         "https://www.google.com/maps/place/BARRAGEM+DOS+CORREDORES/@-5.1071945,-42.0378374,15z",
@@ -68,7 +71,7 @@ final atracoes = [
       "assets/imagens/tur6_3.png",
     ],
     descricao: [
-      "A Catedral de Santo Antônio é um templo católico situado na Praça Bona Primo, em Campo Maior, no estado do Piauí. Desde 12 de junho de 1976, é Sé episcopal[1] da Diocese de Campo Maior.",
+      "A Catedral de Santo Antônio é um marco arquitetônico e espiritual em Campo Maior. Sua imponente estrutura e elementos decorativos atraem visitantes interessados na história e na cultura local. Ela está localizada no centro da cidade, mais precisamente na Praça Bona Primo. Ela tem capacidade para 480 fieis sentados, mas ultrapassa essa meta nas novenas em honra ao padroeiro da cidade, Santo Antônio.É graças ao padroeiro que a cidade transforma-se no período de 31 de maio a 13 de junho. O festejo de Santo Antônio é uma da principais atrações em Campo Maior e atraem turistas de outros estados, além de piauienses de todos as regiões.",
       "Foi erguida sob o comando do pároco Mateus Rufino sobre os escombros da antiga igrejinha de Santo Antônio cuja construção teve início em 1944 e foi concluída em 1962, em evento comemorativo do bicentenário de Campo Maior. É um monumento da Diocese do mesmo município.",
     ],
     localizacao:
--- lib/dados/ondecebs.dart
+++ lib/dados/ondecebs.dart
@@ -22,5 +22,51 @@ final ondecebs = [
       descricao: ["Barzinho com a melhor panelada de Campo Maior"],
       localizacao:
           "https://www.google.com/maps/place/Bar,+Churrascaria+e+Posto+de+Lavagem,+Pernambuco/@-4.8460534,-42.1762464,16z/data=!4m15!1m8!3m7!1s0x7918c37c3ce6d19:0x5c5fc14971920f5a!2sBar,+Churrascaria+e+Posto+de+Lavagem,+Pernambuco!8m2!3d-4.8461011!4d-42.176128!10e5!16s%2Fg%2F11f3qxlh9_!3m5!1s0x7918c37c3ce6d19:0x5c5fc14971920f5a!8m2!3d-4.8461011!4d-42.176128!16s%2Fg%2F11f3qxlh9_?hl=pt&entry=ttu",
-      preco: Preco.barato)
+      preco: Preco.barato),
+  OndeCeB(
+    id: "ceb3",
+    nome: "Misturinha Santo Antônio",
+    fotos: [
+      "assets/imagens/ceb3_1.png",
+      "assets/imagens/ceb3_2.png",
+      "assets/imagens/3deb5b4b-a490-41d4-8ed0-15162b014d39_1.png",
+    ],
+    descricao: [
+      "A Misturinha Santo Antônio é um exemplo do modelo de restaurante popular comum em Campo Maior, que vende pratos de baixo custo, composto de carboidrato (normalmente maria isabel), creme de galinha e uma opção de proteina (a mistura).",
+    ],
+    localizacao:
+        "https://www.google.com/maps/place/Misturinha+Santo+Ant%C3%B4nio/@-4.8232778,-42.1692543,15z/data=!4m2!3m1!1s0x0:0x9416ef7ebbb40ef5?sa=X&ved=2ahUKEwj7x_Lsy_SCAxXgppUCHfJYAJoQ_BJ6BAhEEAA",
+    preco: Preco.ok,
+  ),
+  OndeCeB(
+    id: "ceb4",
+    nome: "Rei da Tábua",
+    fotos: [
+      "assets/imagens/ceb4_1.png",
+      "assets/imagens/ceb4_2.png",
+      "assets/imagens/ceb4_3.png",
+    ],
+    descricao: [
+      "Localizado próximo ao Monumento do Jenipapo, o Restaurante Rei da Tábua busca propiciar a experiência rural e caipira de Campo Maior. O restaurante conta com piscina, áreas cobertas locais instagramáveis para fotos e funciona para o almoço segundas quintas e sextas e de 10h as 20h nos fins de semanas."
+    ],
+    localizacao:
+        "https://www.google.com/maps/place/Restaurante+Rei+da+T%C3%A1bua/@-4.7897269,-42.1165101,15z/?entry=ttu",
+    preco: Preco.ok,
+  ),
+  OndeCeB(
+    id: "ceb5",
+    nome: "Ecosítio Carnaúba",
+    fotos: [
+      "assets/imagens/ceb5_1.png",
+      "assets/imagens/ceb5_2.png",
+      "assets/imagens/ceb5_3.png",
+    ],
+    descricao: [
+      "Localizado no Pé da Serra, o Ecosítio Carnaúba se integra a paisagem, aproveitando as piscinas naturais e a beleza da Serra.",
+      "Funciona exclusivamente nos fins de semana e feriados, de 8:30 às 18:30",
+    ],
+    localizacao:
+        "https://www.google.com/maps/place/Eco+S%C3%ADtio+Carna%C3%BAba/@-4.9463627,-42.2019737,17.17z/data=!4m6!3m5!1s0x79193a6d1de398b:0x4785f5ec9fe29513!8m2!3d-4.9440625!4d-42.2010625!16s%2Fg%2F11r_4tz1yg?entry=ttu",
+    preco: Preco.caro,
+  )
 ];
